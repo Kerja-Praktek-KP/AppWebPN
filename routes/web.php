86f2a9caa5482710f3759c8eafa89139e5c392ad
Panca03 <pancawigu080@gmail.com>
2024-07-22 22:21:19 +0700

    beranda dan profil

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Login');
 });
+
+Route::post('/Login', function () {
+    // Handle login logic here
+
+    // Redirect to home page
+    return redirect('/beranda');
+});
+
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
